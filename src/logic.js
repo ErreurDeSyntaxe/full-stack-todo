051878d5234a9a18ddd6340c53328962bd051095
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-01-16 11:12:40 +0800

    Fix internal logic
    The app is now fully functional. Some projects and tasks have been
    hardcoded to allow for quicker testing. Functions have been added or
    fixed.
    
    I will continue to use only the CLI version until the Local Storage
    functionality is implemented. Afterwards, I will build the UI. This is
    what I should have done from the beginning, as per the Build a House
    From the Inside Out article.
    
    This commit reminds me of the first Odin project (Recipes). I had not
    committed often, despite repeated indications to do so. My long-awaited
    commit broke something and it took a long time to undo the damage. At
    the time, I hope it was a lesson learnt. I now hope the same: I hope
    I will build from the inside out the next time.

--- src/logic.js
+++ src/logic.js
@@ -14,26 +14,53 @@ function todoApp() {
     }
 
     const deleteProject = (string) => {
-        for (let i = 0; i < projectList.length; i++) {
-            if (projectList[i].getName() == string && string != "Inbox") {
-                projectList.splice(i, 1)
-                selectProject("Inbox")
-                break
+        if (string != "Inbox") {
+            for (let i = 0; i < projectList.length; i++) {
+                if (projectList[i].getName() == string) {
+                    projectList.splice(i, 1)
+                    console.log(string + " project deleted. Selecting 'Inbox'")
+                    selectProject("Inbox")
+                    break
+                }
             }
+        } else {
+            console.log("Cannot delete default project 'Inbox'")
         }
     }
 
     const getProjects = () => projectList
 
-    const selectProject = (string) => {
+    const printProjects = () => {
         for (let i = 0; i < projectList.length; i++) {
-            if (projectList[i].getName() == string) {
-                currentProject = i
-                return projectList[i]
+            if (i == currentProject) {
+                if (projectList[i].getTasks().length == 0) {
+                    console.log(">> Project " + i + " : " + projectList[i].getName() + " task: NO TASKS IN THIS PROJECT")
+                }
+                for (let j = 0; j < projectList[i].getTasks().length; j++) {
+                    console.log(">> Project " + i + " : " + projectList[i].getName() + " task: " + j + " " + projectList[i].getTasks()[j].getName())
+                }
+            }
+            else {
+                if (projectList[i].getTasks().length == 0) {
+                    console.log("Project " + i + " : " + projectList[i].getName() + " task: NO TASKS IN THIS PROJECT")
+                }
+                for (let j = 0; j < projectList[i].getTasks().length; j++) {
+                    console.log("Project " + i + " : " + projectList[i].getName() + " task: " + j + " " + projectList[i].getTasks()[j].getName())
+                }
             }
         }
     }
 
+    const selectProject = (string) => {
+        for (let i = 0; i < projectList.length; i++) {
+            if (projectList[i].getName() == string) {
+                console.log(string + " found at index " + i)
+                currentProject = i
+            }
+        }
+    }
+
+    // returns the array index, not the project itself
     const getCurrentProject = () => currentProject
 
     return {
@@ -41,7 +68,8 @@ function todoApp() {
         deleteProject,
         getProjects,
         selectProject,
-        getCurrentProject
+        getCurrentProject,
+        printProjects
     }
 }
 
@@ -56,6 +84,9 @@ function Project(string) {
             }
         }
         taskList.push(Task(string))
+        // for (let i = 0; i < taskList.length; i++) {
+        //     console.log(taskList[i].getName())
+        // }
         return true
     }
 
@@ -95,4 +126,16 @@ function Task(string) {
     return { getName, setName, getDate, setDate, getStatus, setStatus }
 }
 
-export { todoApp }
+// export { todoApp }
+const app = todoApp()
+app.addProject("Inbox")
+app.addProject("Maïté")
+app.addProject("Exercise")
+app.addProject("Reading")
+app.getProjects()[0].addTask("Live and let die")
+app.getProjects()[1].addTask("Drop Off")
+app.getProjects()[1].addTask("Pick Up")
+app.getProjects()[1].addTask("Play")
+app.getProjects()[2].addTask("Jog")
+app.getProjects()[2].addTask("Stretch")
+app.getProjects()[3].addTask("LOTR")
