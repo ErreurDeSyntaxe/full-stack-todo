76e878668b79afe752cf1ca74aa3e84387140951
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-01-25 15:24:01 +0800

    Add module for app's logic

--- src/index.js
+++ src/index.js
@@ -1,151 +1,18 @@
 import './style.css';
 import { buildPage } from './page';
-
-function todoApp() {
-  const projectList = [];
-  const taskList = [];
-  let currentProject = 0;
-
-  const printAll = () => {
-    console.log('\n******Project List:******');
-    for (let i = 0; i < projectList.length; i++) {
-      if (currentProject === i) {
-        console.log(projectList[i].getName() + ' <<<<<<');
-      } else {
-        console.log(projectList[i].getName());
-      }
-    }
-    console.log('\n++++++++Task List:++++++++');
-    for (const task of taskList) {
-      console.log(task.getParent() + ': ' + task.getName());
-    }
-  };
-
-  const addProject = (newProject) => {
-    for (const project of projectList) {
-      if (project.getName() === newProject) {
-        console.log('A project with that name already exists.');
-        return;
-      }
-    }
-    projectList.push(Project(newProject));
-    currentProject = projectList.length - 1;
-    // The newly created project becomes the focus
-  };
-
-  const removeProject = (unwantedProject) => {
-    if (unwantedProject === 'Inbox') {
-      return;
-    }
-    for (let i = 0; i < projectList.length; i++) {
-      if (projectList[i].getName() === unwantedProject) {
-        projectList.splice(i, 1);
-        currentProject = 0; // Back to Inbox project
-        return;
-      }
-    }
-  };
-
-  const selectProject = (wanted) => {
-    for (let i = 0; i < projectList.length; i++) {
-      if (wanted === projectList[i].getName()) {
-        currentProject = i;
-        printAll();
-      }
-    }
-  };
-
-  const getProjects = () => projectList;
-
-  const getCurrentProject = () => currentProject;
-
-  const addTask = (newTask) => {
-    for (const task of taskList) {
-      if (
-        task.getParent() === getProjects()[getCurrentProject()].getName() &&
-        task.getName() === newTask
-      ) {
-        console.log('A task with that name already exists.');
-        return;
-      }
-    }
-    taskList.push(Task(newTask, getProjects()[getCurrentProject()].getName()));
-    console.log(`\n${newTask} was added`);
-  };
-
-  const removeTask = (unwantedTask) => {
-    for (let i = 0; i < taskList.length; i++) {
-      if (
-        taskList[i].getName() === unwantedTask &&
-        taskList[i].getParent() === getProjects()[getCurrentProject()].getName()
-      ) {
-        taskList.splice(i, 1);
-        return;
-      }
-    }
-  };
-
-  const getTasks = () => taskList;
-
-  return {
-    printAll,
-    addProject,
-    removeProject,
-    getCurrentProject,
-    getProjects,
-    selectProject,
-    addTask,
-    removeTask,
-  };
-}
-
-function Project(string) {
-  // const id = 1;
-  let name = string;
-
-  const getName = () => {
-    return name;
-  };
-
-  const setName = (newName) => {
-    name = newName;
-  };
-
-  return { getName, setName };
-}
-
-function Task(taskName, projectName) {
-  // const id = 1;
-  let parentProject = projectName;
-  let name = taskName;
-
-  const getName = () => {
-    return name;
-  };
-
-  const getParent = () => {
-    return parentProject;
-  };
-
-  const setName = (newName) => {
-    name = newName;
-  };
-
-  return { getName, getParent, setName };
-}
+import { todoApp } from './logic';
 
 buildPage();
 const app = todoApp();
+app.readLocally();
 app.addProject('Inbox');
 app.addProject('Exercise');
+app.addTask('Jog 20 minutes');
 app.addProject('Work');
 app.addProject('Reading');
 app.selectProject('Inbox');
 app.addTask('LOTR');
 app.selectProject('Reading');
 app.addTask('LOTR');
-app.selectProject('Exercise');
-app.removeTask('LOTR');
-app.selectProject('Reading');
-app.removeTask('LOTR');
 app.printAll();
+app.storeLocally();
---
+++ src/logic.js
@@ -0,0 +1,182 @@
+function todoApp() {
+  const projectList = [];
+  const taskList = [];
+  let currentProject = 0;
+
+  const printAll = () => {
+    console.log('\n******Project List:******');
+    for (let i = 0; i < projectList.length; i++) {
+      if (currentProject === i) {
+        console.log(projectList[i].getName() + ' <<<<<<');
+      } else {
+        console.log(projectList[i].getName());
+      }
+    }
+    console.log('\n++++++++Task List:++++++++');
+    for (const task of taskList) {
+      console.log(task.getParent() + ': ' + task.getName());
+    }
+  };
+
+  const addProject = (newProject) => {
+    for (const project of projectList) {
+      if (project.getName() === newProject) {
+        console.log(`A project called ${newProject} already exists.`);
+        return;
+      }
+    }
+    projectList.push(Project(newProject));
+    console.log(`New project '${newProject}' added.`);
+    currentProject = projectList.length - 1;
+    // The newly created project becomes the focus
+  };
+
+  const removeProject = (unwantedProject) => {
+    if (unwantedProject === 'Inbox') {
+      return;
+    }
+    for (let i = 0; i < projectList.length; i++) {
+      if (projectList[i].getName() === unwantedProject) {
+        projectList.splice(i, 1);
+        currentProject = 0; // Back to Inbox project
+        return;
+      }
+    }
+  };
+
+  const selectProject = (wanted) => {
+    for (let i = 0; i < projectList.length; i++) {
+      if (wanted === projectList[i].getName()) {
+        currentProject = i;
+        //printAll();
+      }
+    }
+  };
+
+  const getProjects = () => projectList;
+
+  const getCurrentProject = () => currentProject;
+
+  const addTask = (newTask) => {
+    for (const task of taskList) {
+      if (
+        task.getParent() === getProjects()[getCurrentProject()].getName() &&
+        task.getName() === newTask
+      ) {
+        console.log(
+          `A task called ${newTask} in project ${getProjects()[
+            getCurrentProject()
+          ].getName()} already exists.`
+        );
+        return;
+      }
+    }
+    taskList.push(Task(newTask, getProjects()[getCurrentProject()].getName()));
+    console.log(
+      `New task '${newTask}' in project '${getProjects()[
+        getCurrentProject()
+      ].getName()}' added.`
+    );
+  };
+
+  const removeTask = (unwantedTask) => {
+    for (let i = 0; i < taskList.length; i++) {
+      if (
+        taskList[i].getName() === unwantedTask &&
+        taskList[i].getParent() === getProjects()[getCurrentProject()].getName()
+      ) {
+        taskList.splice(i, 1);
+        return;
+      }
+    }
+  };
+
+  const getTasks = () => taskList;
+
+  const storeLocally = () => {
+    // Store the projects
+    const projectNames = projectList.map((project) => {
+      return project.getName();
+    });
+    localStorage.setItem('localProjects', JSON.stringify(projectNames));
+
+    // Store the tasks
+    const taskNames = taskList.map((task) => {
+      const parentName = task.getParent();
+      const taskName = task.getName();
+      const fullName = `${parentName}#$%${taskName}`;
+      return fullName;
+    });
+    localStorage.setItem('localTasks', JSON.stringify(taskNames));
+  };
+
+  const readLocally = () => {
+    // Retrieve the projects from storage
+    const localProjects = JSON.parse(localStorage.getItem('localProjects'));
+    console.log(localProjects);
+    if (!localProjects) return; // There's nothing in storage. Abort.
+    localProjects.forEach((project) => {
+      addProject(project); // Add each name found in storage as a project
+    });
+
+    // Retrieve the tasks from storage
+    const localTasks = JSON.parse(localStorage.getItem('localTasks'));
+    console.log(localTasks);
+    if (!localTasks) return; // There's nothing in storage. Abort.
+    localTasks.forEach((item) => {
+      const projectTask = item.split('#$%');
+      selectProject(projectTask[0]); // Select the project first
+      addTask(projectTask[1]); // Then add the task to the project
+    });
+  };
+
+  return {
+    printAll,
+    addProject,
+    removeProject,
+    getCurrentProject,
+    getProjects,
+    selectProject,
+    addTask,
+    removeTask,
+    storeLocally,
+    readLocally,
+  };
+}
+
+function Project(string) {
+  // const id = 1;
+  let name = string;
+
+  const getName = () => {
+    return name;
+  };
+
+  const setName = (newName) => {
+    name = newName;
+  };
+
+  return { getName, setName };
+}
+
+function Task(taskName, projectName) {
+  // const id = 1;
+  let parentProject = projectName;
+  let name = taskName;
+
+  const getName = () => {
+    return name;
+  };
+
+  const getParent = () => {
+    return parentProject;
+  };
+
+  const setName = (newName) => {
+    name = newName;
+  };
+
+  return { getName, getParent, setName };
+}
+
+export { todoApp };
